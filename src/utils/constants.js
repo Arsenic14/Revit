28a88917b83e22669bc6fe07ec316089da8626fc
Arsenic14 <77527319+Arsenic14@users.noreply.github.com>
2022-10-20 11:30:21 +0530

    Revit

---
+++ src/utils/constants.js
@@ -0,0 +1,56 @@
+
+import ScienceIcon from '@mui/icons-material/Science';
+import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
+import SportsBasketballIcon from '@mui/icons-material/SportsBasketball';
+import CodeIcon from '@mui/icons-material/Code';
+import FunctionsIcon from '@mui/icons-material/Functions';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import CableIcon from '@mui/icons-material/Cable';
+import EngineeringIcon from '@mui/icons-material/Engineering';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import BusinessIcon from '@mui/icons-material/Business';
+import PodcastsIcon from '@mui/icons-material/Podcasts';
+import LightbulbIcon from '@mui/icons-material/Lightbulb';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'Automobiles', icon: <DirectionsCarIcon />, },
+  { name: 'Basketball', icon: <SportsBasketballIcon />, },
+  { name: 'Competitive Coding', icon: <CodeIcon />, },
+  { name: 'Mathematics', icon: <FunctionsIcon />, },
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'Finance', icon: <MonetizationOnIcon />, },
+  { name: 'Electronics', icon: <CableIcon />, },
+  { name: 'Engineering', icon: <EngineeringIcon />, },
+  { name: 'Motivation', icon: <EmojiEmotionsIcon />, },
+  { name: 'Fitness', icon: <FitnessCenterIcon />, },
+  { name: 'Startups', icon: <BusinessIcon />, },
+  { name: 'Podcasts', icon: <PodcastsIcon />, },
+  { name: 'Veritasium', icon: <ScienceIcon />, },
+  { name: 'Deewane IES', icon: <LightbulbIcon />, },
+  { name: 'Abdul Bari', icon: <CodeIcon />, },
+  { name: 'Love Babbar', icon: <CodeIcon />, },
+  { name: '3Blue1Brown', icon: <FunctionsIcon /> },
+  { name: 'Khan GS Research', icon: <ScienceIcon />, },
+  { name: 'StudyIQ IAS', icon: <PsychologyIcon />, },
+  { name: 'TED', icon: <PsychologyIcon />, },
+  { name: 'Vsauce', icon: <ScienceIcon />, },
+  { name: 'Kurzgesagt', icon: <PsychologyIcon />, },
+  { name: 'Lesics', icon: <PsychologyIcon />, },
+  { name: 'RBI Grade B', icon: <MenuBookIcon />, },
+  { name: 'JEE', icon: <MenuBookIcon />, },
+  { name: 'Think School', icon: <PsychologyIcon />, },
+  { name: 'Dhruv Rathee', icon: <LightbulbIcon />, },
+
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
